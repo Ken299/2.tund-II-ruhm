0f256650cade1a2bbd445ce4535281356841bf51
Ken Aonurm <kenaon@greeny.cs.tlu.ee>
2016-02-10 10:49:34 +0200

    Singleton Pattern

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
- 
+	(function(){
+		"use strict";
+		
+		
+		var Moosipurk = function(){
+			
+			//Singleton pattern (4 rida)
+			if(Moosipurk.instance){
+				return Moosipurk.instance;
+			}
+			Moosipurk.instance = this; // this viitab 
+			
+			
+			console.log('moosipurgi sees');
+		};
+		
+		
+		window.onload = function(){
+			var app = new Moosipurk();
+		};
+		
+	})();
